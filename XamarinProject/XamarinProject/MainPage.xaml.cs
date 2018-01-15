0f89956f7772e7f3a9a98730b5a34ac0c1f8c21e
HeadHoffer <kim.1996hoffmann@gmail.com>
2018-01-15 17:00:47 +0200

    Simple checkbox

--- XamarinProject/XamarinProject/MainPage.xaml.cs
+++ XamarinProject/XamarinProject/MainPage.xaml.cs
@@ -9,9 +9,27 @@ namespace XamarinProject
 {
 	public partial class MainPage : ContentPage
 	{
+        bool isChecked = false;
+
 		public MainPage()
 		{
 			InitializeComponent();
 		}
+
+        void OnChecked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            if(!isChecked)
+            {
+                button.Image = "Assets/CheckedCheckBox.png";
+                isChecked = true;
+            }
+
+            else
+            {
+                button.Image = "Assets/EmptyCheckBox.png";
+                isChecked = false;
+            }
+        }
 	}
 }
