dcfc41b9e41292c42354fed7639e705560e9ef74
Montonen Sinja TTV15SP <sinjamontonen@kamk.fi>
2018-01-22 17:25:22 +0200

    leikin vähän layouttien kanssa WIPWIPWIP

---
+++ XamarinProject/XamarinProject/Testing.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinProject
+{
+	public partial class Testing : ContentPage
+	{
+		public Testing ()
+		{
+			//InitializeComponent ();
+
+		}
+	}
+}
